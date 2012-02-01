c8ea07bd25fa2cba3bce09f5fdfd3c5a1019b7ca
Mr. Bougo <bougospam@gmail.com>
2012-02-01 17:17:59 +0100

    list files always relative to their root

--- ghostfs-list.pl
+++ ghostfs-list.pl
@@ -41,6 +41,7 @@ my %types_itos = (
 sub make_findfile {
 	my ($root,$fdesc) = @_;
 
+	chdir $root;
 	find( sub{
 		my $path = $File::Find::name;
 		my @stat = lstat $_;
@@ -57,7 +58,7 @@ sub make_findfile {
 		local $, = "\000";
 		local $\ = "\000\n";
 		print $fdesc ($path, "$stat[4]:$stat[5]", @times, $stat[7], $typechr, $target, $perms);
-	}, $root);
+	}, '.');
 }
 
 my $dir = $ARGV[0];
